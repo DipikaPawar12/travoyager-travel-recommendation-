260abc6445cc9489e7fe8cd0f7406bd742ab9ef2
dipikapawar <dipikapawar2001@gmail.com>
2021-03-18 19:02:13 +0530

    User register file is linked with home screen

--- travel_recommand/travel/urls.py
+++ travel_recommand/travel/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
+    url(r'^userRegister$', userRegister, name='userRegister'),
 ]
