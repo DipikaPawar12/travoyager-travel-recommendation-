dc1e93b29d2fbcb6652bf6752f381168e55417e9
Frency-Chauhan <frencykchauhan@gmail.com>
2021-03-19 11:28:09 +0530

    Added Comment

--- travel_recommand/travel/models.py
+++ travel_recommand/travel/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from enum import Enum
-# Create your models here.
+# Create your models here....
+# Comment
 class User(models.Model):
     username = models.CharField(max_length=200,unique=True)
     password = models.CharField(max_length=15)
