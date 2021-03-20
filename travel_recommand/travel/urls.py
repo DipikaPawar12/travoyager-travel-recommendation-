0541d394ae5596a902634e5e41ae6089c025b678
mansi-ctrl <60286748+mansi-ctrl@users.noreply.github.com>
2021-03-20 23:07:12 +0530

    Quite changed in models.py, Comments added in view.py and Splitted userInput HTML file

--- travel_recommand/travel/urls.py
+++ travel_recommand/travel/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^userRegister$', userRegister, name='userRegister'),
     url(r'^userInput$', userInput, name='userInput'),
     url(r'^userLogin$', userLogin, name='userLogin'),
+    url(r'^split_userInput$', split_userInput, name='split_userInput'),
 
 ]
